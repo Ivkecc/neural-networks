804d31faec93710510d8924e3f385d74d6f80950
Luka Ivankovic <ivkec@Lukas-MacBook-Air-2.local>
2026-06-27 12:14:44 +0200

    Implement HAR dataset and dataloader

---
+++ src/data/dataloaders.py
@@ -0,0 +1,30 @@
+from torch.utils.data import DataLoader
+
+from config.config import BATCH_SIZE, DATASET_ROOT
+from datasets.har_dataset import HARDataset
+
+
+def create_dataloaders(batch_size: int = BATCH_SIZE):
+    train_dataset = HARDataset(
+        root=DATASET_ROOT,
+        split="train",
+    )
+
+    test_dataset = HARDataset(
+        root=DATASET_ROOT,
+        split="test",
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+    )
+
+    return train_loader, test_loader
